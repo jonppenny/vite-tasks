1e2d05bf7c3579ad9835a1a0b79dce9e7a423963
Jonathan Penny <jonppenny@users.noreply.github.com>
2025-11-09 20:43:27 +0000

    Refactor Vite config for Laravel integration

--- assets/vite.config.mjs
+++ assets/vite.config.mjs
@@ -1,42 +1,14 @@
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        js: 'js/main.js',
-        scss: 'scss/main.scss',
-      },
-      output: {
-        entryFileNames: chunk => 'js/main.js',
-        assetFileNames: assetInfo => {
-          const name = assetInfo?.name || assetInfo?.info?.sourceFilename || '';
-          if (name.endsWith('.css')) {
-            return 'css/main.css';
-          }
-          return 'assets/[name][extname]';
-        },
-      },
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        sassOptions: {
-          quietDeps: true,
-        },
-      },
-    },
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        { src: 'fonts/*', dest: 'fonts' },
-        { src: 'images/*', dest: 'images' },
-      ],
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ['css/app.scss', 'js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        outDir: 'path/to/dist/'
+    }
 });
